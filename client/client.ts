f12a9e4ff97e33b0ad6f9edbdf00eac097171b9d
Ryan Oliveira <jryancoelho79@hotmail.com>
2026-01-29 13:52:32 -0300

    Implement client streaming for stock price updates and enhance client functionality
    - Added support for client streaming in the StockPrice client to update stock prices using the UpdateStockPriceClientStreaming method.
    - Integrated readline and fs modules to read stock price data from a file and send updates in a streaming manner.
    - Updated package.json to include readline as a dependency and added a new stockprices.txt file for testing stock price updates.
    - Commented out previous unary and server streaming implementations for clarity and focus on the new client streaming functionality.

--- client/client.ts
+++ client/client.ts
@@ -1,39 +1,62 @@
 import { ChannelCredentials } from '@grpc/grpc-js'
 import { StockPriceClient } from './proto/stock_market_grpc_pb'
-import { StockRequest } from './proto/stock_market_pb'
+import { StockRequest, UpdateStockPriceRequest } from './proto/stock_market_pb'
+import * as readline from 'readline'
+import * as fs from 'fs'
 
 const target = '0.0.0.0:8080'
 
 const client = new StockPriceClient(target, ChannelCredentials.createInsecure())
 
 function main() {
-  const request = new StockRequest().setSymbol('PETRA4')
+  // const request = new StockRequest().setSymbol('PETRA4')
 
   let deadline = new Date();
   deadline.setSeconds(deadline.getSeconds() + 8);
 
 
-  client.getStockPrice(request, { deadline: deadline } as any, (err, response) => {
+  // client.getStockPrice(request, { deadline: deadline } as any, (err, response) => {
+  //   if (err) return console.log(err)
+  //   console.log("[Unary]Preço da ação: ", response.toObject())
+  // })
+
+  // const call = client.getStockPriceServerStreaming(request, {
+  //   deadline: deadline,
+  // }
+  // )
+
+  // call.on('data', (response) => {
+  //   console.log("[Server Streaming]Preço da ação: ", response.toObject())
+  // })
+  // call.on('error', (err) => {
+  //   console.log("Error on server streaming: ", err)
+  // })
+  // call.on('status', (status) => {
+  //   console.log("Status: ", status)
+  // })
+  // call.on('end', () => {
+  //   console.log("End of stream")
+  // })
+
+  const callUpdate = client.updateStockPriceClientStreaming({deadline: deadline}, (err, response) => {
     if (err) return console.log(err)
-    console.log("[Unary]Preço da ação: ", response.toObject())
+
+    console.log("[Client Streaming]Preço da ação: ", response.toObject()) 
   })
 
-  const call = client.getStockPriceServerStreaming(request, {
-    deadline: deadline,
-  }
-  )
+  const fileStream = fs.createReadStream('data/stockprices.txt')
+  const rl = readline.createInterface({
+    input: fileStream,
+    output: process.stdout,
+    terminal: false
+  })
 
-  call.on('data', (response) => {
-    console.log("[Server Streaming]Preço da ação: ", response.toObject())
+  rl.on('line', (line) => {
+    const data = JSON.parse(line)
+    callUpdate.write(new UpdateStockPriceRequest().setSymbol(data.symbol).setPrice(data.price))
   })
-  call.on('error', (err) => {
-    console.log("Error on server streaming: ", err)
-  })
-  call.on('status', (status) => {
-    console.log("Status: ", status)
-  })
-  call.on('end', () => {
-    console.log("End of stream")
+  rl.on('close', () => {
+    callUpdate.end()
   })
 }
 
