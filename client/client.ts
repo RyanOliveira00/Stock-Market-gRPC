da03be787565aaa26161a088c0888fedaf1165d1
Ryan Oliveira <jryancoelho79@hotmail.com>
2026-01-28 23:35:18 -0300

    Enhance client to support server streaming for stock price updates
    - Updated the client implementation to include server streaming capabilities for stock price retrieval.
    - Added logging for both unary and server streaming responses to improve clarity.
    - Set a deadline for stock price requests to manage response timing effectively.

--- client/client.ts
+++ client/client.ts
@@ -7,10 +7,33 @@ const target = '0.0.0.0:8080'
 const client = new StockPriceClient(target, ChannelCredentials.createInsecure())
 
 function main() {
-  client.getStockPrice(new StockRequest().setSymbol('PETRA4'), (err, response) => {
+  const request = new StockRequest().setSymbol('PETRA4')
+
+  let deadline = new Date();
+  deadline.setSeconds(deadline.getSeconds() + 8);
+
+
+  client.getStockPrice(request, { deadline: deadline } as any, (err, response) => {
     if (err) return console.log(err)
-      console.log("Response: ", response)
-    console.log("Preço da ação: ", response.toObject())
+    console.log("[Unary]Preço da ação: ", response.toObject())
+  })
+
+  const call = client.getStockPriceServerStreaming(request, {
+    deadline: deadline,
+  }
+  )
+
+  call.on('data', (response) => {
+    console.log("[Server Streaming]Preço da ação: ", response.toObject())
+  })
+  call.on('error', (err) => {
+    console.log("Error on server streaming: ", err)
+  })
+  call.on('status', (status) => {
+    console.log("Status: ", status)
+  })
+  call.on('end', () => {
+    console.log("End of stream")
   })
 }
 
