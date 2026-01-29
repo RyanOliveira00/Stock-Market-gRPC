bf42c851c1d2239b099b35528a101adc37056ac4
Ryan Oliveira <jryancoelho79@hotmail.com>
2026-01-28 21:17:15 -0300

    Refactor client implementation to use a main function and update stock symbol
    - Introduced a main function to encapsulate the stock price retrieval logic.
    - Changed the stock symbol from 'AAPL' to 'PETRA4' for the stock price request.
    - Improved response logging for better clarity.

--- client/client.ts
+++ client/client.ts
@@ -2,9 +2,16 @@ import { ChannelCredentials } from '@grpc/grpc-js'
 import { StockPriceClient } from './proto/stock_market_grpc_pb'
 import { StockRequest } from './proto/stock_market_pb'
 
-const client = new StockPriceClient('0.0.0.0:8080', ChannelCredentials.createInsecure())
+const target = '0.0.0.0:8080'
 
-client.getStockPrice(new StockRequest().setSymbol('AAPL'), (err, response) => {
-  if (err) return console.log(err)
-  console.log(response.toObject())
-})
+const client = new StockPriceClient(target, ChannelCredentials.createInsecure())
+
+function main() {
+  client.getStockPrice(new StockRequest().setSymbol('PETRA4'), (err, response) => {
+    if (err) return console.log(err)
+      console.log("Response: ", response)
+    console.log("Preço da ação: ", response.toObject())
+  })
+}
+
+main()
